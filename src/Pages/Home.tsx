707ce60428078b22b53df7e97046c6a779f4498f
M Daffa A <daffa.alghifary62@gmail.com>
2026-02-09 21:02:01 +0700

    feat: Setup routing and rainbowkit

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,10 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit"
+
+export default function Home() {
+    return(
+        <div>
+            <h1 className="text-2xl font-semibold text-blue-500">HOME</h1>
+            <ConnectButton/>
+        </div>
+    )
+}
